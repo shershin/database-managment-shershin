f20f3930e661b3666f67d48934bb884f5027eb13
michael shershin <michael.shershin@gmail.com>
2014-09-29 11:56:08 -0400

    shershin lab 4 ready to be viewed
    query 5 may or may not work properly and i have no idea why

---
+++ lab4.sql
@@ -0,0 +1,80 @@
+﻿--one--
+select distinct city
+from agents
+where aid in 
+	(select distinct aid
+	from orders 
+	where cid in 
+		(select cid
+		from customers
+		where name = 'Tipthelop')
+	)
+order by city asc;
+
+--two--
+select distinct pid
+from orders
+where aid in 
+	(select aid
+	from orders
+	where cid in 
+		(select cid
+		from customers
+		where city = 'Kyoto')
+	)
+order by pid asc;
+
+--three--
+select cid, name
+from customers
+where cid not in 
+	(select cid
+	from orders
+	where aid = 'a04')
+order by cid asc;
+
+--four--
+select cid, name
+from customers
+where cid in 
+	(
+	(select distinct cid
+	from orders
+	where pid ='p01')
+		intersect
+	(select cid
+	from orders
+	where pid = 'p07')
+	)
+order by cid asc;
+
+--five--
+select distinct pid
+from orders
+where cid in
+	(select cid
+	from orders
+	where aid = 'a04')
+order by pid asc;
+
+--six--
+select name, discount
+from customers
+where cid in 
+	(select cid
+	from orders
+	where aid in
+		(select aid
+		from agents
+		where city = 'Dallas' or city = 'Newark')
+	)
+order by name asc;
+
+--seven--
+select *
+from customers
+where city != 'Dallas'and city != 'Kyoto'and discount in 
+	(select discount
+	from customers
+	where city = 'Dallas' or city = 'Kyoto')
+order by cid asc;
