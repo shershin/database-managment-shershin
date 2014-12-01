c6f905560db2f340818b9cdf83c43fa8fe6a7286
shershin <michael.shershin@gmail.com>
2014-12-01 13:13:42 -0500

    lab 10
    i am not sure if they work as well as i was hoping i ran out of time to
    test them how i wanted and make sure they work but i do have two working
    already for my project

---
+++ lab10.sql
@@ -0,0 +1,25 @@
+﻿--michael shershin
+--lab 10
+create function preReqNum(num integer out num integer,
+					     out name text,
+					     out credits integer) returns set of record as 
+$$
+begin
+      return query
+      select c.num, c.name, c.credits
+      from   Prerequisites p, Courses c
+      where  p.courseNum = c.num and p.preReqNum = c.num and p.preReqNum = $1;
+end;
+$$ 
+language plpgsql;
+
+create or replace function isPreReqFor(courseNum int) returns int as 
+$$
+begin
+      select Prerequisites.preReqNum
+      from   Prerequisites
+       where  Prerequisites.courseNum = $1;
+end;
+$$ 
+language plpgsql;
+
